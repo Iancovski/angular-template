d74b5d7113387b7dc9ebf120a8087615b65043e4
Iancovski <46848500+Iancovski@users.noreply.github.com>
2024-05-11 14:38:33 -0300

    Added Eslint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.html"],
-  theme: {
-    extend: {      
-      colors: {
-        'primary': '#03A9F4',
-        'accent': '#607D8B',
-        'warn': '#FF5722'
-      }
-    }
-  },
-  plugins: [],
-}
+    content: ['./src/**/*.html'],
+    theme: {
+        extend: {      
+            colors: {
+                'primary': '#03A9F4',
+                'accent': '#607D8B',
+                'warn': '#FF5722'
+            }
+        }
+    },
+    plugins: [],
+};
 
