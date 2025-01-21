81aedee4686245c20b9a58611f5a752d74a2756b
Iancovski <46848500+Iancovski@users.noreply.github.com>
2025-01-20 21:13:12 -0300

    Angular 19
    [*] Updated dependencies
    [+] Added style folder with default files and mixin
    [+] Added eslint
    [+] Added prettier

---
+++ src/app/services/scroll.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+    providedIn: "root",
+})
+export class ScrollService {
+    scrollTo(id: string) {
+        const element = document.getElementById(id);
+
+        if (element) {
+            element.scrollIntoView({ behavior: "smooth", block: "start" });
+        }
+    }
+}
